f06343a78c4bc77f8eadd497031f5a7cd6a4f44e
flyingninja314 <drw.ninja@gmail.com>
2014-09-12 13:55:33 -0700

    MIlestone 12 yooooooo
    LeArNiNg aBoUt sInGlEtOnS AnD SuCh

--- app/src/main/java/android/wrightd/crystalball/Predictions.java
+++ app/src/main/java/android/wrightd/crystalball/Predictions.java
@@ -8,4 +8,11 @@ public class Predictions {
 
     }
 
+    public static Predictions get(){
+        if(predictions == null){
+            predictions = new Predictions;
+        }
+        return predictions;
+    }
+
 }
