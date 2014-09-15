2d017541f32e078dd42d7f2c9a5d9aa471709573
flyingninja314 <drw.ninja@gmail.com>
2014-09-15 14:12:06 -0700

    Milestone 13
    YOOOOOOOOOOOOOOO we got a new variable and its s string thing
    can you even spell thoh
    T-H-O-T
    shut up

--- app/src/main/java/android/wrightd/crystalball/Predictions.java
+++ app/src/main/java/android/wrightd/crystalball/Predictions.java
@@ -2,10 +2,13 @@ package android.wrightd.crystalball;
 
 public class Predictions {
 
-    public static Predictions predictions;
+    private static Predictions predictions;
+    private String[] answers;
 
     private Predictions(){
-
+        answers = new String[]{
+            "Answer is hazy. $0.99 could clear that up, though. :D"
+        };
     }
 
     public static Predictions get(){
